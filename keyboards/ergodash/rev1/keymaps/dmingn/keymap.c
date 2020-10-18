5f76c530ae51248d0aafe1fb9fd0f4eb3ab5fcd1
dmingn <dmingn@users.noreply.github.com>
2020-10-19 03:24:26 +0900

    swap up and down

--- keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
+++ keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |  Del |                    | Bksp |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | Ctrl |||||||| LoMh | Space| Space|||||||| Enter| Enter| RaHe |||||||| Left | Down |  Up  | Right|
+   * | Ctrl |  GUI |  ALt | Ctrl |||||||| LoMh | Space| Space|||||||| Enter| Enter| RaHe |||||||| Left |  Up  | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,                         KC_UNDS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                         KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOMH,    KC_SPC, KC_SPC,         KC_ENT, KC_ENT,  RAHE,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOMH,    KC_SPC, KC_SPC,         KC_ENT, KC_ENT,  RAHE,             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
   ),
 
   /* Lower
