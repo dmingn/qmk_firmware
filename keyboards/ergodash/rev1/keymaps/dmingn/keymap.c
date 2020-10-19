0ec75635117625209b504ff5f36ca5e433856962
dmingn <dmingn@users.noreply.github.com>
2020-10-20 03:05:51 +0900

    add capslock

--- keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
+++ keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | Ctrl |||||||| LoMh | Space|  Del |||||||| Bksp | Enter| RaHe |||||||| Left |  Up  | Down | Right|
+   * | Caps |  GUI |  ALt | Ctrl |||||||| LoMh | Space|  Del |||||||| Bksp | Enter| RaHe |||||||| Left |  Up  | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JU_LBRC,                   JU_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JU_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_PGUP,                   KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    JU_SCLN, JU_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                    KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,          LOMH,    KC_SPC,  KC_SPC,  KC_SPC,  KC_ENT,  RAHE,             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
+    KC_CAPS, KC_LGUI, KC_LALT, KC_LCTL,          LOMH,    KC_SPC,  KC_SPC,  KC_SPC,  KC_ENT,  RAHE,             KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower
