a8a33f9d824b37d14c1a90a22eea6b5e86f6d45a
dmingn <dmingn@users.noreply.github.com>
2020-10-24 03:51:43 +0900

    modify raise layer

--- keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
+++ keyboards/ergodash/rev1/keymaps/dmingn/keymap.c
@@ -93,23 +93,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Raise
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
+   * | ____ |      |      |      |      |      |      |                    |      | Nlck |   7  |   8  |   9  |   /  | ____ |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * | ____ |      |      |      |      |      |      |                    |      |      |   4  |   5  |   6  |   *  | ____ |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   :  |  "   |
+   * | ____ |      |      |      |      |      |      |                    |      |   =  |   1  |   2  |   3  |   -  | ____ |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * | ____ |      |      |      |      |      | ____ |                    | ____ | Enter|   0  |      |   .  |   +  | ____ |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Home |PageUp|PageDn|  End |
+   * | ____ | ____ | ____ | ____ |||||||| Lower| ____ | ____ |||||||| ____ | ____ | Raise|||||||| Home |PageUp|PageDn|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                   KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                   KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-    KC_TAB,  KC_1,    JU_2,    KC_3,    KC_4,    KC_5,    KC_DEL,                    KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT,  \
-    KC_LSFT, JU_6,    JU_7,    JU_8,    JU_9,    JU_0,    KC_SPC,                    KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC,  KC_DEL,  KC_BSPC, KC_ENT,  RAISE,            KC_HOME, KC_PGUP, KC_PGDN, KC_END   \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_PEQL, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                   _______, KC_PENT, KC_P0,   XXXXXXX, KC_PDOT, KC_PPLS, _______, \
+    _______, _______, _______, _______,          LOWER,   _______, _______, _______, _______, RAISE,            KC_HOME, KC_PGUP, KC_PGDN, KC_END   \
   ),
 
   /* Adjust
